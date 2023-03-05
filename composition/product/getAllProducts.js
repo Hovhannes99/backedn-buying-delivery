523ae434cabfb46327f19c4192bfd9f98b30c5ca
HOhanjanyan <hovoohanjanyan9work@gmail.com>
2023-03-05 23:43:34 +0400

    add search

--- composition/product/getAllProducts.js
+++ composition/product/getAllProducts.js
@@ -3,11 +3,17 @@ const Product = require("../../models/Product");
 
 const getAllProducts = async (req, res) => {
     try {
-        const { variant } = req.body;
-        console.log(variant)
-        const data = variant ? await Product.find({variant}): await Product.find() ;
-        if (data){
-            return res.status(201).json(data)
+        const { variant, searchValue } = req.body;
+        if (!!searchValue){
+            const searchResult =  await Product.find({ title: {$regex : '.*'+searchValue.toLowerCase()+'.*', $options: "$i"}}).exec();
+            if (searchResult){
+                return res.status(201).json(searchResult)
+            }
+        }else {
+            const data = variant ? await Product.find({variant}): await Product.find();
+            if (data){
+                return res.status(201).json(data)
+            }
         }
         return res.status(402).json({errors:{message: "something goes wrong"}})
     }catch (e){
